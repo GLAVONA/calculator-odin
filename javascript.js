d3cc7e7550183509d228e9677ce2dc5907657b92
GLAVONA <97589269+GLAVONA@users.noreply.github.com>
2022-11-15 15:52:18 +0200

    fixed logic/algorithm

--- javascript.js
+++ javascript.js
@@ -1,3 +1,15 @@
+const buttons = document.querySelectorAll("button");
+const result = document.querySelector(".result")
+const preResult = document.querySelector(".pre-result");
+const operators = ["+","-","*","/"]
+let numberA = 0;
+let numberB = 0;
+let operator = "";
+let answer = 0;
+let equalPressed = false;
+let numbersRegex = /(\d*\.?\d+)\D(\d*\.?\d+)/;
+let decimal = false;
+
 function add(a,b) {
     return a+b;
 };
@@ -26,3 +38,84 @@ function operate(operator, a, b){
             return divide(a,b);
     }
 }
+
+function allClear(){
+        preResult.textContent="";
+        result.textContent="";
+        answer = 0;
+        operator = "";
+}
+
+function getNumbers(){
+    let matchedNumbers = preResult.textContent.match(numbersRegex);
+    numberA = parseFloat(matchedNumbers[1]);
+    numberB = parseFloat(matchedNumbers[2]);
+}
+
+buttons.forEach(button=>{
+    button.addEventListener('click', populatePreResult=>{
+        
+        if (button.className === "number") {
+            if (equalPressed)
+            {
+                allClear();
+                equalPressed=false;
+                decimal = false;
+            }
+            equalPressed=false;
+        preResult.textContent += button.value;      
+        }
+
+        if (button.className==="operator"){
+            if (preResult.textContent!==""){
+                let lastChar = preResult.textContent.at(-1);
+                if (!operators.some(op=>preResult.textContent.includes(op)) && lastChar !=="."){
+                    preResult.textContent += button.value;    
+                    operator = button.value;
+                    decimal=false;
+                }
+                else if(operators.includes(lastChar)) {
+                    preResult.textContent = preResult.textContent.slice(0, -1) + button.value;
+                    operator = button.value;
+                    decimal=false;
+                }
+            }
+            if (result.textContent!=="" && equalPressed){
+                operator = button.value;
+                preResult.textContent = result.textContent + operator;
+                equalPressed=false;         
+                decimal=false;
+            }
+            else{
+
+            }
+        }
+
+        if (button.className === "all-clear"){
+            allClear()
+            decimal = false;
+        }
+
+        if (button.className==="equal-sign"){
+            equalPressed=true;
+            getNumbers();
+            answer = operate(operator,numberA,numberB).toFixed(15);
+            result.textContent = parseFloat(answer);
+            decimal = false;
+        }
+
+        if(button.className === "decimal"){
+            
+            if (!decimal){
+                preResult.textContent += ".";
+                decimal=true;
+                if(equalPressed){
+                    preResult.textContent = "";
+                    preResult.textContent += ".";
+                    equalPressed=false;
+                }
+            }
+        }
+
+    });
+})
